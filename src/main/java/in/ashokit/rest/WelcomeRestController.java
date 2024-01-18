1b8ef788f726e051764f7197c9a3ca81b2623bc8
rommel2306 <102067929+rommel2306@users.noreply.github.com>
2024-01-17 23:41:24 -0500

    Update WelcomeRestController.java

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -18,7 +18,7 @@ public class WelcomeRestController {
 	@GetMapping
 	public String welcomeMsg() {
 		logger.info("***** welcomeMsg() execution start *****");
-		String msg = "Welcome to Ashok IT - Software Training Institute (HYD) ..!!";
+		String msg = "Welcome to Rommel jefrin vlogs!!!";
 		logger.info("***** welcomeMsg() execution end *****");
 		return msg;
 	}
